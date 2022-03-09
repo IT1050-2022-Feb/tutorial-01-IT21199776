3f960ea2dc72213ad921d01ec3e3aa1df7f9a0e4
IT21199776 <101172884+IT21199776@users.noreply.github.com>
2022-03-09 10:01:40 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -13,6 +13,29 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+  int num, count=1, sum=0;
+  printf("Enter a positive number: ");
+  scanf("%d", &num);
+  
+  printf("Sum of natural numbers from 1 to %d is: \n);
+  while(count <= num)
+  {
+    sum = sum + count;
+    printf("%d", count;
+    count++;
+           
+           if(count>num)
+           {
+             printf("= %d\n", sum);
+           }
+           else
+           {
+             printf("+");
+           }
+  }
+  
+  printf("\nSum = %d\n", sum);
+  
   
   return 0;
 }
